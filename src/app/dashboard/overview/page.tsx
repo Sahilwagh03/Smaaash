10bf1c23a51004e40e86a5c53ecb6205efdfbe1e
sahil wagh <sahilwagh142@gmail.com>
2025-02-12 21:09:59 +0530

    overview fix

--- src/app/dashboard/overview/page.tsx
+++ src/app/dashboard/overview/page.tsx
@@ -48,8 +48,7 @@ const OverView = () => {
         </div>
     )
 }
-
-export const StatCard = ({ title, value, change, icon: Icon }: StatCardProps) => {
+const StatCard = ({ title, value, change, icon: Icon }: StatCardProps) => {
     return (
         <Card>
             <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
